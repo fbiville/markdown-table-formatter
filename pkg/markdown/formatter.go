5a5d5dde7c08e39a0bbc8c23837a123b7f9520c9
fbiville@pivotal.io <florent.biville@gmail.com>
2019-08-19 23:02:29 +0200

    Add pretty-print implementation

--- pkg/markdown/formatter.go
+++ pkg/markdown/formatter.go
@@ -9,12 +9,14 @@ type TableFormatter interface {
 	Format(data [][]string) (string, error)
 }
 
-type config struct {
-	headers []string
-}
-
 func NewDefaultTableFormatter(headers []string) TableFormatter {
 	return &defaultTableFormatter{
+		config: &config{headers: headers},
+	}
+}
+
+func NewPrettyTableFormatter(headers []string) TableFormatter {
+	return &prettyTableFormatter{
 		config: &config{headers: headers},
 	}
 }
@@ -25,27 +27,71 @@ type defaultTableFormatter struct {
 
 func (formatter *defaultTableFormatter) Format(data [][]string) (string, error) {
 	builder := &strings.Builder{}
-	formatter.appendHeaders(builder)
-	if err := formatter.appendData(builder, data); err != nil {
-		return "", err
+	appendHeaders(builder, formatter.config.headers)
+	for rowIndex, row := range data {
+		if err := formatter.config.validateRow(rowIndex, row); err != nil {
+			return "", err
+		}
+		builder.WriteString(joinValues(row))
 	}
 	return builder.String(), nil
 }
 
-func (formatter *defaultTableFormatter) appendHeaders(builder *strings.Builder) {
-	columnNames := formatter.config.headers
-	builder.WriteString(joinValues(columnNames))
-	builder.WriteString(joinValues(replaceWith(columnNames, "-")))
+type prettyTableFormatter struct {
+	config *config
 }
 
-func (formatter *defaultTableFormatter) appendData(builder *strings.Builder, data [][]string) error {
-	headerCount := len(formatter.config.headers)
+type prettyTable struct {
+	widths  []int
+	content [][]string
+}
+
+func (formatter *prettyTableFormatter) Format(data [][]string) (string, error) {
+	prettyTable, err := formatter.preComputeFormattedData(data)
+	if err != nil {
+		return "", err
+	}
+	builder := &strings.Builder{}
+	appendHeaders(builder, formatter.config.headers)
+	for _, row := range prettyTable.content {
+		builder.WriteString(joinValues(replacePadded(row, prettyTable.widths)))
+	}
+	return builder.String(), nil
+}
+
+func (formatter *prettyTableFormatter) preComputeFormattedData(data [][]string) (*prettyTable, error) {
+	var widths []int
+	for _, header := range formatter.config.headers {
+		widths = append(widths, len(header))
+	}
 	for rowIndex, row := range data {
-		rowLength := len(row)
-		if rowLength != headerCount {
-			return fmt.Errorf("expected %d column(s), row number %d got %d", headerCount, 1+rowIndex, rowLength)
+		if err := formatter.config.validateRow(rowIndex, row); err != nil {
+			return nil, err
 		}
-		builder.WriteString(joinValues(row))
+		for columnIndex, cell := range row {
+			cellLength := len(cell)
+			if cellLength > widths[columnIndex] {
+				widths[columnIndex] = cellLength
+			}
+		}
+	}
+	return &prettyTable{widths: widths, content: data}, nil
+}
+
+func appendHeaders(builder *strings.Builder, headers []string) {
+	builder.WriteString(joinValues(headers))
+	builder.WriteString(joinValues(replaceRepeated(headers, "-")))
+}
+
+type config struct {
+	headers []string
+}
+
+func (config *config) validateRow(rowIndex int, row []string) error {
+	headerCount := len(config.headers)
+	rowLength := len(row)
+	if rowLength != headerCount {
+		return fmt.Errorf("expected %d column(s), row number %d got %d", headerCount, 1+rowIndex, rowLength)
 	}
 	return nil
 }
@@ -54,10 +100,19 @@ func joinValues(values []string) string {
 	return fmt.Sprintf("| %s |\n", strings.Join(values, " | "))
 }
 
-func replaceWith(items []string, replacementSymbol string) []string {
+func replaceRepeated(items []string, replacementSymbol string) []string {
 	var result []string
 	for _, item := range items {
 		result = append(result, strings.Repeat(replacementSymbol, len(item)))
+	}
+	return result
+}
+
+func replacePadded(items []string, widths []int) []string {
+	var result []string
+	for columnIndex, item := range items {
+		format := fmt.Sprintf("%%-%ds", widths[columnIndex])
+		result = append(result, fmt.Sprintf(format, item))
 	}
 	return result
 }
--- pkg/markdown/formatter_test.go
+++ pkg/markdown/formatter_test.go
@@ -37,3 +37,33 @@ var _ = Describe("Default Markdown table markdown", func() {
 		Expect(err).To(MatchError("expected 3 column(s), row number 1 got 2"))
 	})
 })
+
+var _ = Describe("Pretty Markdown table markdown", func() {
+
+	var formatter markdown.TableFormatter
+
+	BeforeEach(func() {
+		formatter = markdown.NewPrettyTableFormatter([]string{"column 1", "column 2", "column 3"})
+	})
+
+	It("pretty-prints accordingly", func() {
+		output, err := formatter.Format([][]string{
+			{"value 1", "val 2", "v 3"},
+		})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(output).To(Equal(strings.TrimLeft(`
+| column 1 | column 2 | column 3 |
+| -------- | -------- | -------- |
+| value 1  | val 2    | v 3      |
+`, "\n")))
+	})
+
+	It("fails to format if the data dimension does not match the headers", func() {
+		_, err := formatter.Format([][]string{
+			{"value 1", "value 2"},
+		})
+
+		Expect(err).To(MatchError("expected 3 column(s), row number 1 got 2"))
+	})
+})
