aa06a2b779c996e19b298c111fe5bad11f67bea3
Florent Biville <florent.biville@gmail.com>
2021-12-15 13:34:01 +0100

    Migrate away from Ginkgo/Gomega

--- pkg/markdown/formatter_test.go
+++ pkg/markdown/formatter_test.go
@@ -2,84 +2,94 @@ package markdown_test
 
 import (
 	"github.com/fbiville/markdown-table-formatter/pkg/markdown"
-	. "github.com/onsi/gomega"
 	"strings"
-
-	. "github.com/onsi/ginkgo"
+	"testing"
 )
 
-var _ = Describe("Default Markdown table markdown", func() {
+func TestDefaultRendering(st *testing.T) {
+	formatter := markdown.NewTableFormatterBuilder().
+		Build("column 1", "column 2", "column 3")
 
-	var formatter markdown.TableFormatter
-
-	BeforeEach(func() {
-		formatter = markdown.NewTableFormatterBuilder().
-			Build("column 1", "column 2", "column 3")
-	})
-
-	It("formats accordingly", func() {
-		output, err := formatter.Format([][]string{
+	st.Run("formats as-is", func(t *testing.T) {
+		actual, err := formatter.Format([][]string{
 			{"value 1", "val 2", "v 3"},
 		})
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(output).To(Equal(strings.TrimLeft(`
+		if err != nil {
+			t.Errorf("Expected no error but got %v", err)
+		}
+		expected := strings.TrimLeft(`
 | column 1 | column 2 | column 3 |
 | -------- | -------- | -------- |
 | value 1 | val 2 | v 3 |
-`, "\n")))
+`, "\n")
+		if expected != actual {
+			t.Errorf("Expected %q but got %q", expected, actual)
+		}
 	})
 
-	It("fails to format if the data dimension does not match the headers", func() {
+	st.Run("fails to format if the data dimension does not match the headers", func(t *testing.T) {
 		_, err := formatter.Format([][]string{
 			{"value 1", "value 2"},
 		})
 
-		Expect(err).To(MatchError("expected 3 column(s), row number 1 got 2"))
+		expected := "expected 3 column(s), row number 1 got 2"
+		if err == nil || err.Error() != expected {
+			t.Errorf("Expected error with message %s but got %v", expected, err)
+		}
 	})
-})
+}
 
-var _ = Describe("Pretty Markdown table markdown", func() {
+func TestPrettyPrintedRendering(st *testing.T) {
 
-	var formatter markdown.TableFormatter
+	formatter := markdown.NewTableFormatterBuilder().
+		WithPrettyPrint().
+		Build("column 1", "column 2", "column 3")
 
-	BeforeEach(func() {
-		formatter = markdown.NewTableFormatterBuilder().
-			WithPrettyPrint().
-			Build("column 1", "column 2", "column 3")
-	})
-
-	It("pretty-prints accordingly", func() {
-		output, err := formatter.Format([][]string{
+	st.Run("pretty-prints accordingly", func(t *testing.T) {
+		actual, err := formatter.Format([][]string{
 			{"value 1", "val 2", "v 3"},
 		})
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(output).To(Equal(strings.TrimLeft(`
+		if err != nil {
+			t.Errorf("Expected no error but got %v", err)
+		}
+		expected := strings.TrimLeft(`
 | column 1 | column 2 | column 3 |
 | -------- | -------- | -------- |
 | value 1  | val 2    | v 3      |
-`, "\n")))
+`, "\n")
+		if expected != actual {
+			t.Errorf("Expected %q but got %q", expected, actual)
+		}
 	})
 
-	It("pads columns if necessary", func() {
-		output, err := formatter.Format([][]string{
+	st.Run("pads columns if necessary", func(t *testing.T) {
+		actual, err := formatter.Format([][]string{
 			{"long value 1", "val 2", "very long value 3"},
 		})
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(output).To(Equal(strings.TrimLeft(`
+		if err != nil {
+			t.Errorf("Expected no error but got %v", err)
+		}
+		expected := strings.TrimLeft(`
 | column 1     | column 2 | column 3          |
 | ------------ | -------- | ----------------- |
 | long value 1 | val 2    | very long value 3 |
-`, "\n")))
+`, "\n")
+		if expected != actual {
+			t.Errorf("Expected %q but got %q", expected, actual)
+		}
 	})
 
-	It("fails to format if the data dimension does not match the headers", func() {
+	st.Run("fails to format if the data dimension does not match the headers", func(t *testing.T) {
 		_, err := formatter.Format([][]string{
 			{"value 1", "value 2"},
 		})
 
-		Expect(err).To(MatchError("expected 3 column(s), row number 1 got 2"))
+		expected := "expected 3 column(s), row number 1 got 2"
+		if err == nil || err.Error() != expected {
+			t.Errorf("Expected error with message %s but got %v", expected, err)
+		}
 	})
-})
+}
